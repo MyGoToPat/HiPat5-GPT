a2f04c74bd7142678fce365ee63dd8b0b3738b9a
MyGoToPat <any2crds+github@gmail.com>
2025-08-17 23:46:41 -0400

    Import useEffect in LoginPage to stop runtime crash

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
 import { Mail, Lock, Eye, EyeOff, ArrowRight } from 'lucide-react';
+import { useNavigate } from 'react-router-dom';
 import { PatAvatar } from '../../components/PatAvatar';
 import { signInWithPassword, getSession } from '../../lib/auth';
 
