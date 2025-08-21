05e2ffdce2cd82ee18f0c7c0270c2f76b84069c5
MyGoToPat <any2crds+github@gmail.com>
2025-08-21 16:15:15 -0400

    Remove Redundant Header Element

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -6,10 +6,6 @@ import OrgSwitcher from '../components/orgs/OrgSwitcher';
 export default function AppLayout() {
   return (
     <div>
-      <div className="flex items-center justify-end px-4 py-2 bg-white border-b border-gray-200">
-        <TopBar />
-        <OrgSwitcher />
-      </div>
       <div style={{ padding: 16 }}>
         <Outlet />
       </div>
